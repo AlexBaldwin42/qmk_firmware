93a4d280c8b546fc6ca45e4d87880b7617c78b11
blackanode <blackanode@gmail.com>
2021-06-24 19:36:04 -0600

    Initial commit

---
+++ keyboards/crkbd/keymaps/alexbaldwin42/config.h
@@ -0,0 +1,57 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+//#define MASTER_LEFT
+#define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+
+#define COMBO_COUNT 2
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 150
+
+#define RGBLIGHT_SLEEP
+
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+//    #define RGBLIGHT_ANIMATIONS
+    #define RGBLED_NUM 27
+#    define RGBLIGHT_LIMIT_VAL 150
+#    define RGBLIGHT_HUE_STEP 16
+#    define RGBLIGHT_SAT_STEP 32
+#    define RGBLIGHT_VAL_STEP 32
+
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#    define RGBLIGHT_EFFECT_KNIGHT
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+// Reduce size
+#define NO_ACTION_MACRO
---
+++ keyboards/crkbd/keymaps/alexbaldwin42/keycodes.h
@@ -0,0 +1,43 @@
+#pragma once
+
+enum layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
+
+enum custom_keycodes { QWERTY = SAFE_RANGE, LOWER, RAISE, ADJUST, RGBRST };
+
+#define LW_SPC LT(_LOWER, KC_SPC)
+#define LW_BSPC LT(_LOWER, KC_BSPC)
+#define CTRL_TB MT(MOD_LCTL, KC_TAB)
+#define RAI_ENT LT(_RAISE, KC_ENT)
+#define RAI_DEL LT(_RAISE, KC_DEL)
+#define ADJ_QT LT(_ADJUST, KC_QUOT)
+#define ALT_X MT(MOD_LALT, KC_X)
+#define ALT_DOT MT(MOD_LALT, KC_DOT)
+#define ALT_F2 MT(MOD_LALT, KC_F2)
+#define RSFT_F11 MT(MOD_RSFT, KC_F11)
+#define CTL_F10 MT(MOD_LCTL, KC_F10)
+#define ALT_F9 MT(MOD_LALT, KC_F9)
+#define CTL_F1 MT(MOD_LCTL, KC_F1)
+#define RSFT_TIL MT(MOD_RSFT, KC_TILD)
+
+#define CTL_V MT(MOD_LCTL, KC_V) 
+#define SFT_EQ MT(MOD_LSFT, KC_EQL)
+#define SFT_QT MT(MOD_RSFT, KC_QUOT)
+
+#define SFT_A MT(MOD_LSFT, KC_A)
+#define CTL_Z MT(MOD_LCTL, KC_Z)
+
+#define SFT_SCLN MT(MOD_RSFT, KC_SCLN)
+#define CTL_SLSH MT(MOD_RCTL, KC_SLSH)
+
+#define LOW_SPC LT(_LOWER, KC_SPC)
+#define RAI_EQ LT(_RAISE, KC_EQL)
+#define LOW_BSP LT(_LOWER, KC_BSPC)
+#define LOW_DEL LT(_LOWER, KC_DEL)
+#define ADJ_GRV LT(_ADJUST, KC_GRV)
+#define KC_ANGL LSFT(KC_COMM)
+#define KC_ANGR LSFT(KC_DOT)
