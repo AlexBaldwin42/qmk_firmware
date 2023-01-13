4157bfe277b88c0924c7d981a45ae409ce6f5162
Simian <alex@Mandelbrot>
2023-01-12 21:19:37 -0700

    Cleaned up

--- keyboards/bastardkb/charybdis/3x5_3/keymaps/simian/rules.mk
+++ keyboards/bastardkb/charybdis/3x5_3/keymaps/simian/rules.mk
@@ -1,13 +1,6 @@
 #EEPROM_DRIVER = spi
-#POINTING_DEVICE_DRIVER = pmw3389
 POINTING_DEVICE_DRIVER = pmw3389
 ENCODER_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
 MOUSEKEY_ENABLE = yes 
-
-# # AUDIO_SUPPORTED = no        # Audio is not supported
-# RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
-# RGBLIGHT_SUPPORTED = no  # RGB underglow is supported, but not enabled by default
-# RGB_MATRIX_ENABLE = no    # Enable keyboard RGB matrix functionality
-# # #RGB_MATRIX_DRIVER = WS2812
