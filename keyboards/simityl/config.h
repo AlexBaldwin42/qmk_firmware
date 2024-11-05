70abba6421d09ebf4cc1b7d1ea67496d9cc7fa2c
Alex Baldwin <abaldwin@weberbasin.com>
2024-11-05 13:50:30 -0700

    Added line for old splinky versions

--- keyboards/simityl/config.h
+++ keyboards/simityl/config.h
@@ -25,7 +25,8 @@
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_RIGHT above.
-#define SPLIT_HAND_PIN GP15
+//#define SPLIT_HAND_PIN GP13 //splinky2
+#define SPLIT_HAND_PIN GP15 // splinky3
 #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
 
 
